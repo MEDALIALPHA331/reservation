9b6fd7c916245ffda700b0bb3e428351b51e41c0
Med Ali Khaled <medalikhaled331@gmail.com>
2023-11-22 15:19:37 +0100

    install echo, init server, enviroment vars

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # TODOs
 
-init git
-makefile, setup air
 install echo
 init server
 enviroments
+
 models
 extract handler
 init mongo, connect
@@ -13,3 +12,8 @@ global handler
 enviroments
 docker
 pulumi
+
+go get github.com/joho/godotenv
+or import \_ "github.com/joho/godotenv/autoload"
+
+go get github.com/labstack/echo/v4
--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Printf("Hello world \n")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Could not load enviroment variables file, %+v", err)
+	}
+
+	var (
+		PORT = os.Getenv("PORT")
+	)
+
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(200, map[string]string{
+			"message": "it works! 😝",
+		})
+	})
+
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%+v", PORT)))
+
 }
